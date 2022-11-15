c3c4f8dc79c9d1ba0dde74978c4564a40dddb3ff
Gerlenn <gleb_kozlov1998@mail.ru>
2022-11-15 16:50:14 +0300

    unfinished homework

--- app/src/main/java/com/example/androidproject_homework/MainActivity2.kt
+++ app/src/main/java/com/example/androidproject_homework/MainActivity2.kt
@@ -1,27 +1,33 @@
 package com.example.androidproject_homework
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(R.layout.activity_main2)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val btn2 = findViewById<Button>(R.id.btn2)
+        btn2.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity3::class.java)
+            )
+        }
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.item_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    val goToActivity2 = Intent(this, MainActivity2::class.java)
 
         when (item.itemId) {
-            R.id.goAct2 -> startActivity(goToActivity2)
             R.id.close -> finishAffinity()
         }
         return super.onOptionsItemSelected(item)
