afd1ebe1247e5583145e960967ad0232aa9d6daa
Gerlenn <gleb_kozlov1998@mail.ru>
2022-11-22 22:26:22 +0300

    Homework 19 tasks 1,2

--- app/src/main/java/com/example/androidproject_homework/MainActivity.kt
+++ app/src/main/java/com/example/androidproject_homework/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.androidproject_homework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rb1 = findViewById<RadioButton>(R.id.rb1)
+        val rb2 = findViewById<RadioButton>(R.id.rb2)
+        val rb3 = findViewById<RadioButton>(R.id.rb3)
+
+        rb1.setOnClickListener {
+            if (rb1.isChecked) {
+                rb2.isChecked = false
+                rb3.isChecked = false
+            }
+        }
+
+        rb2.setOnClickListener {
+            if (rb2.isChecked) {
+                rb1.isChecked = false
+                rb3.isChecked = false
+
+            }
+        }
+        rb3.setOnClickListener {
+            if (rb3.isChecked) {
+                rb1.isChecked = false
+                rb2.isChecked = false
+            }
+        }
     }
 }
