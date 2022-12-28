a408cb8ffd45fd43307e7fadd7cb5fe2c000df26
Gerlenn <gleb_kozlov1998@mail.ru>
2022-12-28 20:43:09 +0300

    Home work 26 (completed MVP and SP)

--- app/src/main/java/com/example/androidproject_homework/di/PresentationModule.kt
+++ app/src/main/java/com/example/androidproject_homework/di/PresentationModule.kt
@@ -5,6 +5,7 @@ import com.example.androidproject_homework.domain.items.ItemsInteractor
 import com.example.androidproject_homework.presentation.view.MainPresenter
 import com.example.androidproject_homework.presentation.view.auth.LoginPresenter
 import com.example.androidproject_homework.presentation.view.auth.OnBoardingPresenter
+import com.example.androidproject_homework.presentation.view.home.DetailsPresenter
 import com.example.androidproject_homework.presentation.view.home.ItemsPresenter
 import dagger.Module
 import dagger.Provides
@@ -33,5 +34,10 @@ class PresentationModule {
     @Provides
     fun provideItemsPresenter(itemsInteractor: ItemsInteractor): ItemsPresenter {
         return ItemsPresenter(itemsInteractor)
+    }
+
+    @Provides
+    fun provideDetailsPresenter(authInteractor: AuthInteractor): DetailsPresenter {
+        return DetailsPresenter(authInteractor)
     }
 }
